b8206929193ad8edf1a5084ee9ed4063155102eb
Umang A <umanga.eng@gmail.com>
2021-12-14 01:59:32 -0500

    footer created with 2 icon components

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -3,14 +3,16 @@
     <Header />
     <div id="nav"></div>
     <router-view />
+    <Footer />
   </div>
 </template>
 <script>
 import Header from "./components/navigation/Header.vue";
-
+import Footer from "./components/navigation/Footer.vue";
 export default {
   components: {
     Header,
+    Footer,
   },
 };
 </script>
---
+++ portfolio/src/components/navigation/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="Footer" class="absolute bottom-0 border-t h-auto w-screen">
+    <div
+      class="flex flex-col items-center justify-center mt-5 mx-5 h-40 bg-white"
+    >
+      <div class="flex items-center justify-center">
+        <a href="#" class="px-5">
+          <linkedin-icon />
+        </a>
+        |
+        <a href="#" class="px-5">
+          <github-icon />
+        </a>
+      </div>
+      <span class="text-xl font-thin pt-5">email@domain.com</span>
+    </div>
+    <div class="flex items-center justify-center my-2 border-t">
+      <span class="text-lg font-extralight font-sans italic"
+        >A project by Umang Agrawal</span
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import LinkedinIcon from "../icons/Linkedin-icon.vue";
+import GithubIcon from "../icons/Github-icon.vue";
+export default {
+  components: {
+    LinkedinIcon,
+    GithubIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
