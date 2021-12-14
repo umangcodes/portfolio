deb8fed1b6f19f61ab3e32c4183d9e2604360fa9
Umang A <umanga.eng@gmail.com>
2021-12-14 13:25:12 -0500

    updated skills component

--- portfolio/src/components/home/Skills.vue
+++ portfolio/src/components/home/Skills.vue
@@ -1,6 +1,42 @@
-
 <template>
-  <div></div>
+  <div id="skills" class="flex justify-items-center h-screen w-screen">
+    <div
+      class="grid grid-cols-3 ml-5 mr-5 my-10 py-2 w-full justify-items-center"
+    >
+      <div class="grid-start-1">
+        <!-- <img
+          src="https://static.startuptalky.com/2021/10/Top-skills-in-demand-startuptalky.jpg"
+          alt=""
+          class="object-cover h-auto w-auto"
+        /> -->
+        <div class="flex justify-items-center">
+          <span class="text-center font-bold text-9xl">Skills</span>
+        </div>
+      </div>
+      <div class="grid-start-2">
+        <span class="text-xl font-semibold">Technical</span>
+        <ul class="px-5">
+          <li>Vuex</li>
+          <li>Vue-Router</li>
+          <li>Vuetify</li>
+          <li>Nuxt</li>
+          <li>React</li>
+          <li>GIT</li>
+          <li>Rest Api</li>
+          <li>GraphQL</li>
+        </ul>
+      </div>
+      <div class="grid-start-3">
+        <span class="text-xl font-semibold">Others</span>
+        <ul class="px-5">
+          <li>Linux</li>
+          <li>Efficient time management</li>
+          <li>Adaptibility to new tech and processes</li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
